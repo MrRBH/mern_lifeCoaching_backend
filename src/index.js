99e73aa630086b501baa0d12f23cecd6762dd7a2
Rabiulhassan <rabi81450@gmail.com>
2025-01-16 12:01:15 +0500

    Port 2 test added

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ dotenv.config({ path: './.env' });
 // const app = express();
 const port = process.env.PORT || 8100;
 
+const port2 = process.env.PORT || 8100;
+
 // Middleware to parse JSON request bodies
 
 
